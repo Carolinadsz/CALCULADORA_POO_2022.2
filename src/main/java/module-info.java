bfc76c18f751748ba0d64bffd6f9ea6b75031ab8
Daniel02md <gustanunespp157@gmail.com>
2022-12-14 22:08:19 -0300

    Abrindo branche da página de cadastro

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.main.calculadora {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.principal to javafx.fxml;
     exports com.principal;
